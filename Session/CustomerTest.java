a1dc5a86ace2eadebb73b729d09757a7146e3bee
Uffan Mehmood Khan <uffanmehmoodkhan02@gmail.com>
2024-11-29 18:43:16 +0500

    project migration to Maven and tests

---
+++ Session/CustomerTest.java
@@ -0,0 +1,47 @@
+package Session;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CustomerTest {
+
+    @Test
+    public void testCustomerCreation() throws InterruptedException {
+
+    }
+
+    @Test
+    public void testCustomerBilling() {
+
+    }
+
+    @Test
+    void startSession() {
+
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void checkloginCredential() {
+    }
+
+    @Test
+    void addNewCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void viewBill() {
+    }
+
+    @Test
+    void updateCNIC() {
+    }
+}
---
+++ Session/EmployeeTest.java
@@ -0,0 +1,55 @@
+package Session;
+
+import Database.database;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest {
+
+    @Test
+    public void testEmployeeCreation() throws InterruptedException {
+
+    }
+
+    @Test
+    public void testEmployeeSalary() {
+
+    }
+
+    @Test
+    void startSession() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void checkloginCredential() {
+    }
+
+    @Test
+    void addNewCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void updateBillStatus() {
+    }
+
+    @Test
+    void printBillingReport() {
+    }
+
+    @Test
+    void viewExpiredCNIC() {
+    }
+
+    @Test
+    void updateTaxTariff() {
+    }
+}
